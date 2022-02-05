ce91d9d2b4ec83759b40fdee0ab5024dcac4eae2
Minh Ha <mihado@minh.im>
2022-02-06 10:49:34 +1300

    add phone & email

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,42 @@ class MyApp extends StatelessWidget {
             fontSize: 40.0,
             fontWeight: FontWeight.bold));
 
-    const title = Text('Typist',
+    const title = Text('TYPIST',
         style: TextStyle(
             color: Colors.white,
             fontFamily: 'SourceSansPro',
             fontSize: 20,
-            letterSpacing: 2.5,
-            fontWeight: FontWeight.bold));
+            letterSpacing: 2.5));
 
-    const bodyChildren = <Widget>[circleAvatar, name, title];
+    var phone = Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(10.0),
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+        child: Row(children: <Widget>[
+          Icon(Icons.phone, color: Colors.teal.shade900),
+          const SizedBox(width: 10.0),
+          Text('+64 225 225 368',
+              style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20))
+        ]));
+
+    var email = Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(10.0),
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+        child: Row(children: <Widget>[
+          Icon(Icons.email, color: Colors.teal.shade900),
+          const SizedBox(width: 10.0),
+          Text('mihado@minh.im',
+              style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20))
+        ]));
+
+    var bodyChildren = <Widget>[circleAvatar, name, title, phone, email];
 
     return MaterialApp(
       home: Scaffold(
